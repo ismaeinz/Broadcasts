4d4b490f882ce5a792a61b4b75b0745c8abadfe9
ismaeinz <hire_me_ismaein@hotmail.com>
2025-02-01 09:49:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/broadcasts/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.broadcasts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.broadcasts.ui.theme.BroadcastsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BroadcastsTheme {}
+        }
+    }
+}
